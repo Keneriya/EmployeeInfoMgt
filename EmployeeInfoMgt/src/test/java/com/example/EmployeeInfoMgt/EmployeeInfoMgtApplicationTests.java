1108c16a6d349121be5aafc055e7a99635ff3551
Keneriya <kenuts217@gmail.com>
2025-07-11 14:34:03 +0300

    init commit

---
+++ EmployeeInfoMgt/src/test/java/com/example/EmployeeInfoMgt/EmployeeInfoMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeInfoMgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeInfoMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
